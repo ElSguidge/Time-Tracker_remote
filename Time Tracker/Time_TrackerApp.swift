764ff3a38da4ce8aaa18aa2fff9056312f951b2c
Mark McKeon <markmckeon@Marks-MacBook-Pro.local>
2022-12-07 21:39:00 +1100

    Initial Commit

---
+++ Time Tracker/Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_TrackerApp.swift
+//  Time Tracker
+//
+//  Created by Mark McKeon on 7/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
