c3c71e8e7e50dfe507f32b2e9532129735cdbe76
Mark McKeon <markmckeon@Marks-MacBook-Pro.local>
2023-01-10 13:49:31 +1100

    add routing

---
+++ Time Tracker/Routing/ViewRouter.swift
@@ -0,0 +1,20 @@
+//
+//  ViewRouter.swift
+//  Time Tracker
+//
+//  Created by Mark McKeon on 10/1/2023.
+//
+
+import SwiftUI
+
+struct ViewRouter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewRouter_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewRouter()
+    }
+}
